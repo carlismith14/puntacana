8bcd9ca773a3208acde17a1544faa62ab91c70b5
carlismith14 <carli.t.smith1@gmail.com>
2022-12-13 10:37:21 -0500

    new mac updates

--- script.js
+++ script.js
@@ -28,79 +28,25 @@ navLink.forEach(link => link.addEventListener('click', function(){
 
 
 
-// set up text to print, each item in array is new line
-var aText = new Array(
-    "PuntaCana", 
-    );
-    var iSpeed = 100; // time delay of print out
-    var iIndex = 0; // start printing array at this posision
-    var iArrLength = aText[0].length; // the length of the text array
-    var iScrollAt = 20; // start scrolling up at this many lines
-     
-    var iTextPos = 0; // initialise text position
-    var sContents = ''; // initialise contents variable
-    var iRow; // initialise current row
-     
-    function typewriter()
-    {
-     sContents =  '';
-     iRow = Math.max(0, iIndex-iScrollAt);
-     var destination = document.getElementById("typedtext");
-     
-     while ( iRow < iIndex ) {
-      sContents += aText[iRow++] + '<br />';
-     }
-     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos);
-     if ( iTextPos++ == iArrLength ) {
-      iTextPos = 0;
-      iIndex++;
-      if ( iIndex != aText.length ) {
-       iArrLength = aText[iIndex].length;
-       setTimeout("typewriter()", 500);
-      }
-     } else {
-      setTimeout("typewriter()", iSpeed);
-     }
-    }
-    
-    
-    typewriter();
 
 
 
-    var aText = new Array(
-        "Punta Cana", 
-        );
-        var iSpeed = 100; // time delay of print out
-        var iIndex = 0; // start printing array at this posision
-        var iArrLength = aText[0].length; // the length of the text array
-        var iScrollAt = 20; // start scrolling up at this many lines
-         
-        var iTextPos = 0; // initialise text position
-        var sContents = ''; // initialise contents variable
-        var iRow; // initialise current row
-         
-        function typewriter()
-        {
-         sContents =  '';
-         iRow = Math.max(0, iIndex-iScrollAt);
-         var destination = document.getElementById("typedtext");
-         
-         while ( iRow < iIndex ) {
-          sContents += aText[iRow++] + '<br />';
-         }
-         destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos);
-         if ( iTextPos++ == iArrLength ) {
-          iTextPos = 0;
-          iIndex++;
-          if ( iIndex != aText.length ) {
-           iArrLength = aText[iIndex].length;
-           setTimeout("typewriter()", 500);
-          }
-         } else {
-          setTimeout("typewriter()", iSpeed);
-         }
-        }
-        
-        
-        typewriter();
+// Get the button:
+const mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
